39e2a8b46f935d33aa6407b5c7b48c1428852622
ashitosh <ashitosh@cdac.in>
2025-08-13 14:37:57 +0530

    first git commit.

---
+++ AutoNavCom/src/main/java/com/AutoNavCom/AutoNavCom/AutoNavComApplication.java
@@ -0,0 +1,13 @@
+package com.AutoNavCom.AutoNavCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoNavComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoNavComApplication.class, args);
+	}
+
+}
---
+++ AutoNavCom/src/test/java/com/AutoNavCom/AutoNavCom/AutoNavComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoNavCom.AutoNavCom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNavComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
